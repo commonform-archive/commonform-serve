3557ce08c57ed056f5c4bf28de5d113651b74ec8
Kyle Mitchell <kyle@kemitchell.com>
2015-05-29 21:56:18 +0000

    Rename forms routes files

--- source/handler.js
+++ source/handler.js
@@ -3,20 +3,20 @@ var routes = require('routes');
 var url = require('url');
 var uuid = require('uuid');
 
-var bookmarkRoute = require('./routes/bookmarks');
+var bookmarksRoute = require('./routes/bookmarks');
 var digestsRoute = require('./routes/digests');
-var getFormsRoute = require('./routes/get-forms');
+var formsByDigestRoute = require('./routes/forms-by-digest');
+var formsRoute = require('./routes/forms');
 var indexRoute = require('./routes/index');
 var notFoundRoute = require('./routes/not-found');
-var postFormsRoute = require('./routes/post-forms');
 
 function requestHandler(bole, levelup) {
   var level = levelCommonform(levelup);
   var router = routes();
   router.addRoute('/', indexRoute);
-  router.addRoute('/forms', postFormsRoute);
-  router.addRoute('/forms/:digest', getFormsRoute);
-  router.addRoute('/bookmarks/:bookmark', bookmarkRoute);
+  router.addRoute('/forms', formsRoute);
+  router.addRoute('/forms/:digest', formsByDigestRoute);
+  router.addRoute('/bookmarks/:bookmark', bookmarksRoute);
   router.addRoute('/digests', digestsRoute);
   router.addRoute('*', notFoundRoute);
   return function(request, response) {
