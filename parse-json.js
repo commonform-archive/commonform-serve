d4e78f07503e06db9fb90f95244ca3cd1cc5ac4c
Kyle Mitchell <kyle@kemitchell.com>
2015-11-15 01:46:04 -0800

    Inline helper functions

--- parse-json.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(input, callback) {
-  var error
-  var result
-  try {
-    result = JSON.parse(input) }
-  catch (e) {
-    error = e }
-  callback(error, result) }
--- routes/post.js
+++ routes/post.js
@@ -1,36 +1,55 @@
 module.exports = post
 
-var batchForms = require('../batch-forms')
 var concat = require('concat-stream')
 var merkleize = require('commonform-merkleize')
-var parseJSON = require('../parse-json')
 var validate = require('commonform-validate')
 
 function post(bole, level, request, response) {
   request.pipe(concat(function(buffer) {
-    parseJSON(buffer, function(error, form) {
-      // Invalid JSON
-      if (error) {
+    var form
+    var error
+    try {
+      form = JSON.parse(buffer) }
+    catch (e) {
+      error = e }
+    // Invalid JSON
+    if (error) {
+      response.statusCode = 400
+      response.end() }
+    // Valid JSON
+    else {
+      var valid = validate.form(form)
+      // Invalid Common Form
+      if (!valid) {
         response.statusCode = 400
         response.end() }
-      // Valid JSON
+      // Valid Common Form
       else {
-        var valid = validate.form(form)
-        // Invalid Common Form
-        if (!valid) {
-          response.statusCode = 400
-          response.end() }
-        // Valid Common Form
-        else {
-          var merkle = merkleize(form)
-          var location = ( '/forms/' + merkle.digest )
-          var batch = level.batch()
-          batchForms(batch, form, merkle)
-          batch.write(function(error) {
-            if (error) {
-              response.statusCode = 500
-              response.end() }
-            else {
-              response.statusCode = 201
-              response.setHeader('Location', location)
-              response.end() } }) } } }) })) }
+        var merkle = merkleize(form)
+        var location = ( '/forms/' + merkle.digest )
+        var batch = level.batch()
+        batchForms(batch, form, merkle)
+        batch.write(function(error) {
+          if (error) {
+            response.statusCode = 500
+            response.end() }
+          else {
+            response.statusCode = 201
+            response.setHeader('Location', location)
+            response.end() } }) } } })) }
+
+function batchForms(batch, form, merkle) {
+  var stringified = JSON.stringify(form)
+  var digest = merkle.digest
+  batch.put(digest, stringified)
+  form.content
+    .forEach(function(element, index) {
+      if (isChild(element)) {
+        var childForm = element.form
+        var childMerkle = merkle.content[index]
+        batchForms(batch, childForm, childMerkle) } }) }
+
+function isChild(argument) {
+  return (
+    typeof argument === 'object' &&
+    ( 'form' in argument ) ) }
